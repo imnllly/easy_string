4da9951792bbc08cbd330b49895dce7f374cf50d
imnllly <143996748+imnllly@users.noreply.github.com>
2023-10-02 10:01:51 +0300

    file2.cpp

--- file2.cpp
+++ file2.cpp
@@ -4,12 +4,11 @@
 using namespace std; 
 
 void itc_first_end_three(string str) {
-	cin >> str;
+	int len = itc_len(str);
 	char w1 = str[0];
 	char w2 = str[1];
 	char w3 = str[2];
-	int len = str.length();
-	if (str.length() > 5) {
+	if (len > 5) {
 		cout << w1 << w2 << w3 << endl;
 		cout << str[len - 1] << str[len - 2] << str[len - 3];
 	}
@@ -21,9 +20,8 @@ void itc_first_end_three(string str) {
 }
 
 int itc_count_char_in_str(char ch, string str) {
-	cin >> ch >> str;
 	int num = 0;
-	for (int i = 0; i < str.length(); i++) {
+	for (int i = 0; i < itc_len(str); i++) {
 		if (str[i] == ch) num++;
 	}
 	return num;
