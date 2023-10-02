60c1f400c987c0a5d0115cd8aca1ff0f299004c8
imnllly <143996748+imnllly@users.noreply.github.com>
2023-10-02 10:02:35 +0300

    file1.cpp

--- file1.cpp
+++ file1.cpp
@@ -9,14 +9,12 @@ string itc_hello_str(string name) {
 }
 
 long long itc_len(string str) {
-	cin >> str;
 	int len = 0;
 	for (int i = 0; str[i] != '\0'; i++) len++;
 	return len;
 }
 
 void itc_print_copy_str(string str, int number) {
-	cin >> str >> number;
 	string str2 = str;
 	for (int i = 1; i <= number; i++) {
 		cout << str << endl;
