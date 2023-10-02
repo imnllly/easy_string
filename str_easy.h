9ec5e79558215057ffab6381551bed723a4c2f21
imnllly <143996748+imnllly@users.noreply.github.com>
2023-10-02 10:44:01 +0300

    str_easy.h

--- str_easy.h
+++ str_easy.h
@@ -1,8 +1,9 @@
 #ifndef STR_EASY_H_INCLUDED
 #define STR_EASY_H_INCLUDED
+#include <string>
 using namespace std;
 
-void itc_hello_str(string name);
+string itc_hello_str(string name);
 long long itc_len(string str);
 void itc_print_copy_str(string str, int number);
 void itc_first_end_three(string str);
