9be363090420473f53294d1e6674320d594e510b
imnllly <143996748+imnllly@users.noreply.github.com>
2023-10-02 11:08:56 +0300

    file3.cpp

--- file3.cpp
+++ file3.cpp
@@ -4,36 +4,6 @@
 using namespace std;
 
 
-long long itc_len(string str) {
-	int len = 0;
-	for (int i = 0; str[i] != '\0'; i++) len++;
-	return len;
-}
-/*
-void itc_first_end_three(string str) {
-	int len = itc_len(str);
-	char w1 = str[0];
-	char w2 = str[1];
-	char w3 = str[2];
-	if (len > 5) {
-		cout << w1 << w2 << w3 << endl;
-		cout << str[len - 1] << str[len - 2] << str[len - 3];
-	}
-	else {
-		for (int i = 1; i <= len; i++) {
-			cout << w1;
-		}
-	}
-}
-
-int itc_count_char_in_str(char ch, string str) {
-	int num = 0;
-	for (int i = 0; i < itc_len(str); i++) {
-		if (str[i] == ch) num++;
-	}
-	return num;
-} */
-
 string itc_even_place(string str) {
 	string str2{};
 	for (int i = 0; str[i] != '\0'; i++) {
