d2a407ac0858712481107c567417b5dc8a593a69
imnllly <143996748+imnllly@users.noreply.github.com>
2023-10-09 11:10:52 +0300

    file4.cpp

--- file4.cpp
+++ file4.cpp
@@ -3,7 +3,7 @@
 #include "str_easy.h"
 using namespace std;
 
-
+// проверить пустую строку
 string itc_slice_str(string str, int start, int end) {
 	string result = "";
 	int len = itc_len(str);
@@ -20,3 +20,14 @@ bool itc_equal_reverse(string str) {
 	if (str == itc_reverse_str(str)) return 1;
 	return 0;
 }
+
+// доделать функцию !!
+string itc_cmp_str(string str1, string str2, int num) {
+	num--;
+	string str_new{};
+	for (int i = 0; i < itc_len(str1); i++) {
+		str_new += str1[i];
+		if (i == num) str_new += str2;
+	}
+	return str_new;
+} 
