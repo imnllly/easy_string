1d8f94003aedcf5892a628c031cdff8d3c0a9e45
imnllly <143996748+imnllly@users.noreply.github.com>
2023-10-16 00:03:17 +0300

    Add files via upload

---
+++ file5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "str_easy.h"
+using namespace std;
+
+// доделать
+int itc_find_str(string str1, string str2) {
+	int j = 0;
+	for (int i = 0; i < itc_len(str1); i++) {
+		if (str1[i] == str2[j]) j++;
+	}
+	return;
+}
+
+string itc_three_str(string str1, string str2, string str3) {
+	// что это
+}
+
+int itc_max_char_on_end(string str) {
+	// я устала, пока
+}
